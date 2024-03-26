ffff29627b52a993f47f4fab99ab60d46a7bface
UESTC_KS <102127062+KunSsss@users.noreply.github.com>
2024-03-26 19:51:14 +0800

    Update register.v

--- register.v
+++ register.v
@@ -1,17 +1,12 @@
 `timescale 1ns / 1ps
 
 module register(
-	clk,
-	rst,
-	en,				
-	d,				
-	q				
+	input clk,
+	input rst,
+	input en,				
+	input [15:0] d,				
+	output reg [15:0] q				
 );
-
-input clk, rst, en ;
-input wire [15:0] d ;
-output reg [15:0] q ;
-
     always @ (posedge clk or negedge rst) begin
       if (rst == 0) q <= 0 ;
       else 
