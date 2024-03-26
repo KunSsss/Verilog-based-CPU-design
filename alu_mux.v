460e78c4b88609948cea84caabf6e3100971dee4
UESTC_KS <102127062+KunSsss@users.noreply.github.com>
2024-03-26 19:41:09 +0800

    Update alu_mux.v

--- alu_mux.v
+++ alu_mux.v
@@ -1,22 +1,15 @@
 module alu_mux (
-	clk, 
-	rst, 
-	en_in,
-	offset, 
-	rd_q,
-	rs_q,
-	alu_in_sel,
-	alu_a,
-	alu_b,
-	en_out
+	input clk, 
+	input rst, 
+	input en_in,
+	input [7:0] offset, 
+	input [15:0] rd_q,
+	input [15:0] rs_q,
+	input alu_in_sel,
+	output reg [15:0] alu_a,
+	output reg [15:0] alu_b,
+	output reg en_out
 );
-
-input [15:0] rd_q, rs_q ;
-input clk, rst, en_in, alu_in_sel ;
-input [7:0] offset ;
-output reg [15:0] alu_a, alu_b ;
-output reg en_out ;
-
 	always @ (negedge rst or posedge clk) begin
 		if (rst == 1'b0) begin
 			alu_a <= 16'b0000_0000_0000_0000 ;
