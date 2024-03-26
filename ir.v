b6cd131a8a4f1b8b9d721724d50d2b42775036c4
UESTC_KS <102127062+KunSsss@users.noreply.github.com>
2024-03-26 19:46:43 +0800

    Update ir.v

--- ir.v
+++ ir.v
@@ -1,18 +1,11 @@
 module ir(
-	clk,
-	rst,
-	ins,
-	en_in,
-	en_out,
-	ir_out
+	input clk,
+	input rst,
+	input [15:0] ins,
+	input en_in,
+	output reg en_out,
+	output reg [15:0]ir_out
 );
-
-input clk, rst ;
-input [15:0] ins ;
-input en_in ;
-output reg en_out ;
-output reg [15:0] ir_out ;
-
 always @ (posedge clk or negedge rst) begin
 	if (!rst) begin
 		ir_out <= 16'b000000000000 ;
